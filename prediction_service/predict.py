fb3dbbe0a60a19083741e5031cf497e973e6992f
Sagarnil Bose <62485233+Sagarnil-Bose@users.noreply.github.com>
2022-12-16 08:43:04 +0000

    update web-ui

---
+++ prediction_service/predict.py
@@ -0,0 +1,39 @@
+
+import argparse
+import numpy as np
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from sklearn.ensemble import RandomForestClassifier
+
+from utils import load_model
+from datamodel import churnModelFields
+
+#Instance of FastAPI class
+app = FastAPI()
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+
+@app.get('/')
+async def index():
+    return {'healthcheck':'True'}
+
+@app.post("/predict")
+def predict_churn(data: churnModelFields):
+    
+    # Extract data in correct order
+    clf,features=load_model()
+    data_dict = data.dict()
+    to_predict = [data_dict[feature] for feature in features]
+    to_predict=np.array(to_predict).reshape(1, -1)
+    prediction = clf.predict(to_predict)
+    return {"prediction": prediction[0]}
