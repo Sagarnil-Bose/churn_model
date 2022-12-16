5c526dd6e3c8c185581f2486eeb23108361d6346
Sagarnil Bose <62485233+Sagarnil-Bose@users.noreply.github.com>
2022-12-16 07:26:20 +0000

    update_a

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Hands on MLOps Training',
+    author='Sagarnil Bose',
+    license='MIT',
+)
